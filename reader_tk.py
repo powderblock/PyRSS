e168561c77b9bf949f345501484742891527edb5
Blade Nelson <powderblock@gmail.com>
2014-08-26 02:02:57 -0400

    Fixed TK
    Fixed bugs with buttons, improved UI, added opening entries

--- reader_tk.py
+++ reader_tk.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
 import feedparser, os, sys, webbrowser
 from Tkinter import *
 
@@ -7,29 +8,35 @@ except: import pyreadline as readline
 
 path = os.path.join(os.path.expanduser('~'), 'sites.txt')
 
-def callback(text): webbrowser.get('windows-default').open(text)
+class Program(Frame):
+    def __init__(self, *args, **kwargs):
+        Frame.__init__(self, *args, **kwargs)
+        file = open(path, 'r')
+        for line in file:
+            site = feedparser.parse(line)
+            website = Label(text=line)
+            website.pack()
+            for i in range(0, 3):
+                buttonName = i
+                buttonName = Button(text=site['entries'][i]['title'], command = lambda i=i: self.callback(site['entries'][i]['link']))
+                buttonName.pack(padx=30, pady=15)
+            addRSSButton = Button(text="+", command = self.create_window)
+            addRSSButton.pack(side="right")
+    def callback(self, text): webbrowser.get('windows-default').open(text)
+    def create_window(self):
+        t = Toplevel(self)
+        t.wm_title("Add new RSS feed")
 
-root = Tk()
+        b = Button(t, text="Add New Feed", command = self.addNewFeed)
+        b.pack(side="right")
 
-root.wm_title("PyRSS")
-
-class Program:
-    def __init__(self):
-        try:
-            file = open(path, 'r')
-            print("File found at "+path)
-            for line in file:
-                site = {'name:': 'foo', 'site': line};
-                site = feedparser.parse(site['site'])
-                website = Button(text=line)
-                website.pack()
-                for i in range(0, 5):
-                    buttonName = "button"+line
-                    buttonName = Button(text=site['entries'][i]['title'], command = lambda: callback(site['entries'][i]['link']))
-                    buttonName.pack(padx=30, pady=15)
-            file.close()
-        except: print("No file found at "+path)
-
-program = Program()
-mainloop()
-
+        global e
+        e = Entry(t, width=50)
+        e.pack(side="left")
+    def addNewFeed(self): print e.get()
+if __name__ == "__main__":
+    root = Tk()
+    root.wm_title("PyRSS")
+    main = Program(root)
+    main.pack(side="top", fill="both", expand=True)
+    root.mainloop()
