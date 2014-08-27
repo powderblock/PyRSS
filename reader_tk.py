db0a043b6b3623d50a32e26ce4b3f87d71680491
Blade Nelson <powderblock@gmail.com>
2014-08-27 15:23:35 -0400

    Removes class, reorders TK calls
    Removed class Program, also reordered the calls that Tk is making in
    order to fit this new format.

--- reader_tk.py
+++ reader_tk.py
@@ -14,62 +14,58 @@ path = os.path.join(os.path.expanduser('~'), 'sites.txt')
 urls = []
 urlsFull = []
 
-class Program(Frame):
-    def __init__(self, *args, **kwargs):
-        Frame.__init__(self, *args, **kwargs)
-        try:
-            # Open sites.txt
-            file = open(path, 'r')
-            # Go through each line in sites.txt
-            for line in file:
-                line = line.strip()
-                urlsFull.append(line)
-                if line not in urls:
-                    urls.append(line)
-                    # Feed line found in file to feedparser
-                    site = feedparser.parse(line)
-                    # Create labels for each site gotten out of the file
-                    website = Label(text=line)
-                    website.pack()
-                    # Top three entries from the RSS feed
-                    for i in range(0, 3):
-                        buttonName = i
-                        buttonName = Button(text=site['entries'][i]['title'], command = lambda i=i: self.openSite(site['entries'][i]['link']))
-                        buttonName.pack(padx=30, pady=15)
-            file.close()
-            # Make button for adding RSS feeds to 
-            addRSSButton = Button(text="+", command = self.create_window)
-            addRSSButton.pack(side="right")
+root = Tk()
+root.wm_title("PyRSS")
 
-        except (OSError, IOError) as e:
-            # Confirm the error type
-            if (errno.ENOENT == e.errno):
-                # Tell the user the file was not found.
-                print("No file found at: "+path)
-    def openSite(self, text): webbrowser.get().open(text)
+try:
+    # Open sites.txt
+    file = open(path, 'r')
+    # Go through each line in sites.txt
+    for line in file:
+        line = line.strip()
+        urlsFull.append(line)
+        if line not in urls:
+            urls.append(line)
+            # Feed line found in file to feedparser
+            site = feedparser.parse(line)
+            # Create labels for each site gotten out of the file
+            website = Label(root, text=line)
+            website.pack()
+            # Top three entries from the RSS feed
+            for i in range(0, 3):
+                buttonName = i
+                buttonName = Button(root, text=site['entries'][i]['title'], command = lambda i=i: openSite(site['entries'][i]['link']))
+                buttonName.pack(padx=30, pady=15)
+    file.close()
+    # Make button for adding RSS feeds to 
+    addRSSButton = Button(root, text="+", command = lambda: create_window())
+    addRSSButton.pack(side="right")
 
-    # Create new window for adding new RSS feed to file.
-    def create_window(self):
-        t = Toplevel(self)
-        t.wm_title("Add new RSS feed")
+except (OSError, IOError) as e:
+    # Confirm the error type
+    if (errno.ENOENT == e.errno):
+        # Tell the user the file was not found.
+        print("No file found at: "+path)
 
-        newFeedButton = Button(t, text="Add New Feed", command = self.addNewFeed)
-        newFeedButton.pack(side="right")
+def openSite(text): webbrowser.get().open(text)
 
-        global newFeedGet
-        newFeedGet = Entry(t, width=50)
-        newFeedGet.pack(side="left")
+# Create new window for adding new RSS feed to file.
+def create_window():
+    global feed
+    feed = Toplevel()
+    feed.wm_title("Add new RSS feed")
 
-    def addNewFeed(self):
-        add = open(path, 'w')
-        for i in range(0, len(urlsFull)):
-            add.write(urlsFull[i])
-        add.write("\n"+newFeedGet.get())
-        
-        
-if __name__ == "__main__":
-    root = Tk()
-    root.wm_title("PyRSS")
-    main = Program(root)
-    main.pack(side="top", fill="both", expand=True)
-    root.mainloop()
+    newFeedButton = Button(t, text="Add New Feed", command = lambda: addNewFeed())
+    newFeedButton.pack(side="right")
+
+    global newFeedGet
+    newFeedGet = Entry(t, width=50)
+    newFeedGet.pack(side="left")
+
+def addNewFeed():
+    add = open(path, 'w')
+    for i in range(0, len(urlsFull)):
+        add.write(urlsFull[i])
+    add.write("\n"+newFeedGet.get())
+
+mainloop()
