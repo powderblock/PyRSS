5b28f07c14dcc782569ad38017d7d430bdd8ac23
Blade Nelson <powderblock@gmail.com>
2014-08-27 00:51:25 -0400

    Adds ability to create new RSS feeds to sites.txt
    "+" button now adds whatever is typed in the entry widget to sites.txt.
    Adds comments to improve readability. fixes #1

--- reader_tk.py
+++ reader_tk.py
@@ -8,39 +8,65 @@ except: from tkinter import * # python3
 try: import readline
 except: import pyreadline as readline
 
+#Path for saved RSS site feeds.
 path = os.path.join(os.path.expanduser('~'), 'sites.txt')
+
+urls = []
+urlsFull = []
 
 class Program(Frame):
     def __init__(self, *args, **kwargs):
         Frame.__init__(self, *args, **kwargs)
         try:
+            # Open sites.txt
             file = open(path, 'r')
+            # Go through each line in sites.txt
             for line in file:
-                site = feedparser.parse(line)
-                website = Label(text=line)
-                website.pack()
-                for i in range(0, 3):
-                    buttonName = i
-                    buttonName = Button(text=site['entries'][i]['title'], command = lambda i=i: self.callback(site['entries'][i]['link']))
-                    buttonName.pack(padx=30, pady=15)
-                addRSSButton = Button(text="+", command = self.create_window)
-                addRSSButton.pack(side="right")
+                line = line.strip()
+                urlsFull.append(line)
+                if line not in urls:
+                    urls.append(line)
+                    # Feed line found in file to feedparser
+                    site = feedparser.parse(line)
+                    # Create labels for each site gotten out of the file
+                    website = Label(text=line)
+                    website.pack()
+                    # Top three entries from the RSS feed
+                    for i in range(0, 3):
+                        buttonName = i
+                        buttonName = Button(text=site['entries'][i]['title'], command = lambda i=i: self.openSite(site['entries'][i]['link']))
+                        buttonName.pack(padx=30, pady=15)
+            file.close()
+            # Make button for adding RSS feeds to 
+            addRSSButton = Button(text="+", command = self.create_window)
+            addRSSButton.pack(side="right")
 
         except (OSError, IOError) as e:
-            if (errno.ENOENT == e.errno): 
+            # Confirm the error type
+            if (errno.ENOENT == e.errno):
+                # Tell the user the file was not found.
                 print("No file found at: "+path)
-    def callback(self, text): webbrowser.get('windows-default').open(text)
+    def openSite(self, text): webbrowser.get().open(text)
+
+    # Create new window for adding new RSS feed to file.
     def create_window(self):
         t = Toplevel(self)
         t.wm_title("Add new RSS feed")
 
-        b = Button(t, text="Add New Feed", command = self.addNewFeed)
-        b.pack(side="right")
+        newFeedButton = Button(t, text="Add New Feed", command = self.addNewFeed)
+        newFeedButton.pack(side="right")
 
-        global e
-        e = Entry(t, width=50)
-        e.pack(side="left")
-    def addNewFeed(self): print(e.get())
+        global newFeedGet
+        newFeedGet = Entry(t, width=50)
+        newFeedGet.pack(side="left")
+
+    def addNewFeed(self):
+        add = open(path, 'w')
+        for i in range(0, len(urlsFull)):
+            add.write(urlsFull[i])
+        add.write("\n"+newFeedGet.get())
+        
+        
 if __name__ == "__main__":
     root = Tk()
     root.wm_title("PyRSS")
