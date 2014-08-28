12776478b28eff19bd8f16aefd2c11dfc5b18d2c
Caleb Jones <bionicle.dragon@gmail.com>
2014-08-27 19:11:21 -0700

    Make file automatically if it does not exist
    Remove readline import (this is Tk, we don't need console)
    Remove coding line (we don't use any UTF-8 characters)
    Add appdirs (find application data folder on various OSes)
    - Add appdirs import
    Handle files as list
    Replace "if not in, do stuff" with "if in, skip" for cleanliness
    Change name from "add" to "f" (clearer)

--- reader_tk.py
+++ reader_tk.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python
-# -*- coding: utf-8 -*-
 import feedparser
 import os
 import sys
@@ -9,8 +8,7 @@ import errno
 try: from Tkinter import * # python2
 except: from tkinter import * # python3
 
-try: import readline
-except: import pyreadline as readline
+import appdirs
 
 
 #Path for saved RSS site feeds.
@@ -18,45 +16,46 @@ path = os.path.join(os.path.expanduser('~'), 'sites.txt')
 
 urls = []
 buttons = []
+text = []
 
 root = Tk()
 root.wm_title("PyRSS")
 
+# Open sites.txt
 try:
-    # Open sites.txt
-    file = open(path, 'r')
-    # Go through each line in sites.txt
-except (OSError, IOError) as e:
-    # Confirm the error type
-    if (errno.ENOENT == e.errno):
-        # Tell the user the file was not found.
-        print("No file found at: " + path)
-    sys.exit()
+    with open(path, 'r') as f:
+        text = f.readlines()
+except: pass
 
-for line in file:
+# Go through each line in sites.txt
+for line in text:
     line = line.strip()
-    if line not in urls:
-        urls.append(line)
-        # Feed line found in file to feedparser
-        site = feedparser.parse(line)
-        # Create labels for each site gotten out of the file
-        website = Label(root, text=line)
-        website.pack()
-        num = min(3, len(site['entries']))
-        # Top three entries from the RSS feed
-        for entry in site['entries'][:num]:
-            title = entry['title']
-            callback = lambda link=entry['link']: openSite(link)
-            buttons.append(Button(root, text=title, command=callback))
-            buttons[-1].pack(padx=30, pady=15)
-        noMoreEntries = Label(root, text="No more entries!")
-        noMoreEntries.pack(padx=5, pady=5)
-file.close()
+    # we don't want to process the same URL more than once
+    if line in urls: continue
+
+    urls.append(line)
+    # Feed line found in file to feedparser
+    site = feedparser.parse(line)
+    # Create labels for each site gotten out of the file
+    website = Label(root, text=line)
+    website.pack()
+
+    num = min(3, len(site['entries']))
+    # Top three entries from the RSS feed
+    for entry in site['entries'][:num]:
+        title = entry['title']
+        callback = lambda link=entry['link']: openSite(link)
+        buttons.append(Button(root, text=title, command=callback))
+        buttons[-1].pack(padx=30, pady=15)
+    noMoreEntries = Label(root, text="No more entries!")
+    noMoreEntries.pack(padx=5, pady=5)
+
 # Make button for adding RSS feeds to 
 addRSSButton = Button(root, text="+", command=lambda: create_window())
 addRSSButton.pack(side="right")
 
-def openSite(text): webbrowser.get().open(text)
+def openSite(text): 
+    webbrowser.get().open(text)
 
 # Create new window for adding new RSS feed to file.
 def create_window():
@@ -72,9 +71,9 @@ def create_window():
     newFeedGet.pack(side="left")
 
 def addNewFeed():
-    # Open the file for appending and add the new line to it
-    with open(path, 'a') as add:
-        add.write(newFeedGet.get() + "\n")
+    # Open the file for appending and just write the new line to it
+    with open(path, 'a') as f:
+        f.write(newFeedGet.get() + "\n")
         feed.destroy()
 
 root.mainloop()
